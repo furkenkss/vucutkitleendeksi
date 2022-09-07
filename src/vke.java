c2462e51c70df2bfd921d20eb59d2f1bbaf82a11
furkenkss <beytullah.turhan@gmail.com>
2022-09-07 20:40:49 +0300

    done

--- src/vke.java
+++ src/vke.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class vke {
+import java.util.Scanner;
+//Kilo (kg) / Boy(m) * Boy(m)
+public class vke {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+
+        float kilo,boy;
+
+        System.out.print("Kilo bilgisini giriniz: ");
+        kilo=inp.nextFloat();
+        System.out.print("Boy bilgisini (metre cinsinden ör:1,85) giriniz: ");
+        boy=inp.nextFloat();
+
+        double endeks=kilo/(boy*boy);
+
+        System.out.print("Vücut Kitle Endeksi: "+endeks);
+
+    }
 }
